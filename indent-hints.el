4a1f2e565635162c2d59d19bf05a19f7ebbf6349
Mitchel Humpherys <mitch.special@gmail.com>
2022-10-21 13:23:41 -0700

    indent-hints.el: Add URL file header

--- indent-hints.el
+++ indent-hints.el
@@ -6,6 +6,7 @@
 ;; Author: Mitchel Humpherys <mitch.special@gmail.com>
 ;; Keywords: convenience
 ;; Version: 0.2
+;; URL: https://github.com/mgalgs/indent-hints-mode
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
